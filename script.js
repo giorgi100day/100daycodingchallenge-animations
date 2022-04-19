a4556634a6b8a93d667adba285bbc67295e0c8ab
giorgi100day <103732065+giorgi100day@users.noreply.github.com>
2022-04-19 18:48:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// const turn = document.querySelector(".turn");
+// const bulbuka = document.querySelector(".bulbuka");
+
+// turn.addEventListener("click", function () {
+//   console.log("aq iyo sparta");
+//   bulbuka.classList.toggle("aqana");
+// });
